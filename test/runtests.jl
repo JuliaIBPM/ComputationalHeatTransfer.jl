2101196a32a68ce5155c7f173fcb6e495473bb6c
Jeff Eldredge <jdeldre@g.ucla.edu>
2021-05-10 09:35:41 -0700

    Added basic empty runtests.jl

---
+++ test/runtests.jl
@@ -0,0 +1,33 @@
+using ComputationalHeatTransfer
+using Test
+##using TestSetExtensions
+using Literate
+using Suppressor
+
+const GROUP = get(ENV, "GROUP", "All")
+
+notebookdir = "../examples"
+docdir = "../docs/src/manual"
+litdir = "./literate"
+
+if GROUP == "All" || GROUP == "Auxiliary"
+  #include("pointforce.jl")
+end
+
+
+if GROUP == "All" || GROUP == "Notebooks"
+  for (root, dirs, files) in walkdir(litdir)
+    for file in files
+      #endswith(file,".jl") && startswith(file,"6") && Literate.notebook(joinpath(root, file),notebookdir)
+      endswith(file,".jl") && Literate.notebook(joinpath(root, file),notebookdir)
+    end
+  end
+end
+
+if GROUP == "Documentation"
+  for (root, dirs, files) in walkdir(litdir)
+    for file in files
+      endswith(file,".jl") && Literate.markdown(joinpath(root, file),docdir)
+    end
+  end
+end
