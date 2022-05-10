44f0db7590a719330480a6ddd405c8aa04176922
Yuxuan Li <yuxuanli@Yuxuans-MacBook-Air.local>
2022-05-09 18:29:19 -0700

    Update PreProcessing.jl
    initialize liquid slugs in given number, expected charge ratio, and standard deviation of charge ratios.

--- src/intube/PreProcessing.jl
+++ src/intube/PreProcessing.jl
@@ -1,28 +1,31 @@
 export randomXp
 
-function randomXp(L;numofslugs=32,chargeratio=0.46)
+function randomXp(tube;numofslugs=32,chargeratio=0.46,σ_charge=0.01)
 
-    # L = sys.tube.L
+    L = tube.L
+    Lmin = tube.d
+
+    σ_persection = σ_charge*L/sqrt(numofslugs)
 
     L_perslug=L/numofslugs*chargeratio
     L_persection=L/numofslugs
 
-    # Ls = (randn(numofslugs)/4 .+ 1).*L_perslug
-    if chargeratio <= 0.5
-        Ls = 2*rand(numofslugs).*L_perslug
-    else
-        Ls = ((2*chargeratio-1) .+ rand(numofslugs).*(2-2*chargeratio)).*L_persection
-    end
-# rand(numofslugs)
-    # println(chargeratio)
+    # println(L_persection,σ_persection)
+    # if chargeratio <= 0.5
+        Ls = abs.(randn(numofslugs).*σ_persection .+ L_perslug)
+    # else
+    #     Ls = ((2*chargeratio-1) .+ rand(numofslugs).*(2-2*chargeratio)).*L_persection
+    # end
+
+    # println(Ls)
 
     Xp1s = zeros(numofslugs);
     Xp2s = deepcopy(Xp1s);
 
-    if minimum(Ls) > 0 && maximum(Ls) < L/numofslugs
+    if minimum(Ls) > Lmin && maximum(Ls) < L_persection
 
         for i = 1:length(Xp1s)
-            Xp1s[i] = (i-1)*L/numofslugs
+            Xp1s[i] = (i-1)*L_persection
             Xp2s[i] = Xp1s[i] + Ls[i]
         end
 
