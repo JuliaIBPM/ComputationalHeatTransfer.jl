84b025addcbbe7d2cfde6da479ca7fbfa10659c5
Yuxuan Li <liyuxuan48@live.com>
2022-10-19 22:38:55 -0700

    Update TimeMarching.jl

--- src/intube/TimeMarching.jl
+++ src/intube/TimeMarching.jl
@@ -1,4 +1,4 @@
-export ODE_innertube
+export ODE_innertube,ODE_steadyfilm
 
 function ODE_innertube(u,p,t)
 
@@ -11,6 +11,22 @@ function ODE_innertube(u,p,t)
     newsys = getcurrentsys(u,p)
 
     dynamicsdu = dynamicsmodel(u[1:index_dynamics_end-1],newsys)
+
+    liquiddu = duliquidθtovec(liquidmodel(newsys))
+
+    du = [dynamicsdu;liquiddu]
+
+    return(du)
+
+end
+
+function ODE_steadyfilm(u,p,t)
+
+    index_dynamics_end = findfirst(x->abs(x+1e10) <= 10^(-1), u)
+
+    newsys = getcurrentsys(u,p)
+
+    dynamicsdu = dynamicsmodel_steadyfilm(u[1:index_dynamics_end-1],newsys)
 
     liquiddu = duliquidθtovec(liquidmodel(newsys))
 
