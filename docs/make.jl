19d313d22f2d4a88ca95a2ff59af97b756f25701
Albert <azhou689@g.ucla.edu>
2024-01-25 13:55:58 -0800

    added notebooks and markdown files

--- docs/make.jl
+++ docs/make.jl
@@ -9,16 +9,9 @@ makedocs(
     modules = [ViscousFlow],
     pages = [
         "Home" => "index.md",
-        "Manual" => ["manual/1.-Basic-viscous-flow.md",
-                     "manual/2.-Basic-flow-with-a-stationary-body.md",
-                     "manual/3.-Applying-pulse-forcing-to-a-flow.md",
-                     "manual/4.-Multiple-stationary-bodies.md",
-                     "manual/5.-Moving-reference-frame.md",
-                     "manual/6.-Viscous-flow-about-a-moving-body.md",
-                     "manual/7.-Variable-free-stream.md",
-                     "manual/8.-Lid-driven-cavity.md",
-                     "manual/9.-Exogenous-degrees-of-freedom.md",
-                     "manual/functions.md"
+        "Manual" => ["manual/heatconduction_dirichlet.md",
+                    "manual/heatconduction_neumann.md",
+                    "manual/heatconduction_unbounded.md"
                      ]
         #"Internals" => [ "internals/properties.md"]
     ],
